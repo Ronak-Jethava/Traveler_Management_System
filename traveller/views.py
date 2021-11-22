64bbd7c32cdc4e96f1e99203f57b3812ec06ab78
Ronak-Jethava <ronakjethava1511@gmail.com>
2021-11-22 21:22:11 +0530

    travel_history added

--- traveller/views.py
+++ traveller/views.py
@@ -1,6 +1,8 @@
 from django.http.response import Http404
 from django.shortcuts import render
 from dummy.models import User
+import psycopg2
+from decouple import config
 
 # Create your views here.
 def home(request):
@@ -8,9 +10,26 @@ def home(request):
 
 def travel_history(request):
     if request.method == "POST":
-        user_id = request.POST.get('user_id')
-        user = User.objects.get(pk=user_id)
-        objs = User.objects.select_related('traveller')
-        return render(request, 'get_user_id.html')
+        user_id = int(request.POST.get('user_id'))
+        conn = psycopg2.connect(
+            database=config('DB_NAME'), user=config('DB_USER'), password=config('DB_PASSWORD'), host='127.0.0.1', port= '5432'
+        )
+        c = conn.cursor()
+        c.execute('''
+            select "Scheduled_on".start_date, (select string_agg("Places".place_name, ', ') from "Places" where "Tour Package".package_id = package_id group by package_id), "Tour Package".vehicle, t1.payment_amount
+            from (
+                select "Booking".booking_id, (select "Payment".payment_amount from "Payment" where "Payment".booking_id = "Booking".booking_id and "Payment".payment_status='successful'), "Booking".package_detail_id
+                from "Booking" 
+                where traveller_id = {0}
+                ) as t1(booking_id, payment_amount, schedule_id) 
+                join "Scheduled_on"
+                on t1.schedule_id = "Scheduled_on".schedule_id
+                join "Tour Package"
+                on "Tour Package".package_id = "Scheduled_on".package_id
+       '''.format(user_id))
+        travel_history = c.fetchall()
+        c.close()
+
+        return render(request, 'get_user_id.html', {'travel_history':travel_history})
     else:
         return render(request, 'get_user_id.html')
